ef17c3b861a238a279cb373eb8536316651ac803
lwch <liwenchao@jkstack.com>
2020-12-30 14:20:50 +0800

    支持删除pid文件

--- background.go
+++ background.go
@@ -13,21 +13,24 @@ var cmd *exec.Cmd
 
 // Start start daemon
 func Start(pid string, arg ...string) {
+	chExit := make(chan struct{})
 	if len(pid) > 0 {
 		c := make(chan os.Signal)
 		signal.Notify(c, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
 		go func() {
-			<-c
+			sig := <-c
+			cmd.Process.Signal(sig)
+			<-chExit
 			os.Remove(pid)
 		}()
 	}
 	for {
-		run(pid, arg...)
+		run(chExit, pid, arg...)
 	}
 }
 
-func run(pid string, arg ...string) {
-	cmd := exec.Command(os.Args[0], arg...)
+func run(ch chan struct{}, pid string, arg ...string) {
+	cmd = exec.Command(os.Args[0], arg...)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	cmd.Stdin = os.Stdin
@@ -37,6 +40,9 @@ func run(pid string, arg ...string) {
 		}
 		cmd.Process.Release()
 		cmd.Wait()
+		ch <- struct{}{}
+	} else {
+		os.Exit(1)
 	}
 }
 
