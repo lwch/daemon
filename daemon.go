a62ff3a0c3dc9a37dd9eacbbf3374ed8c62992f5
lwch <liwenchao@jkstack.com>
2020-12-30 11:26:51 +0800

    first commit

---
+++ daemon.go
@@ -0,0 +1,27 @@
+package daemon
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+)
+
+// Start start daemon
+func Start(pid string, arg ...string) {
+	cmd := exec.Command(os.Args[0], arg...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Stdin = os.Stdin
+	if err := cmd.Start(); err == nil {
+		if len(pid) > 0 {
+			writePidFile(pid, cmd.Process.Pid)
+		}
+		cmd.Process.Release()
+		os.Exit(0)
+	}
+}
+
+func writePidFile(dir string, pid int) {
+	ioutil.WriteFile(dir, []byte(fmt.Sprintf("%d", pid)), 0644)
+}
