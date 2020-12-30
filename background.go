e703cc213512380bfa47ea4b7cdba603b57cbac9
lwch <liwenchao@jkstack.com>
2020-12-30 14:53:02 +0800

    支持username参数

--- background.go
+++ background.go
@@ -6,41 +6,59 @@ import (
 	"os"
 	"os/exec"
 	"os/signal"
+	"os/user"
 	"syscall"
 )
 
 var cmd *exec.Cmd
 
 // Start start daemon
-func Start(pid string, arg ...string) {
+func Start(pid, username string, arg ...string) {
 	chExit := make(chan struct{})
+	onExit := false
 	if len(pid) > 0 {
 		c := make(chan os.Signal)
 		signal.Notify(c, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
 		go func() {
 			sig := <-c
+			onExit = true
 			cmd.Process.Signal(sig)
 			<-chExit
 			os.Remove(pid)
+			os.Exit(0)
 		}()
 	}
 	for {
-		run(chExit, pid, arg...)
+		run(chExit, pid, username, arg...)
+		if onExit {
+			return
+		}
 	}
 }
 
-func run(ch chan struct{}, pid string, arg ...string) {
+func run(ch chan struct{}, pid, username string, arg ...string) {
 	cmd = exec.Command(os.Args[0], arg...)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	cmd.Stdin = os.Stdin
-	if err := cmd.Start(); err == nil {
-		if len(pid) > 0 {
-			writePidFile(pid, cmd.Process.Pid)
+	if len(user) > 0 {
+		u, err := user.Lookup(username)
+		if err != nil {
+			fmt.Println("user not found")
+			os.Exit(1)
 		}
+		cmd.SysProcAttr = &syscall.SysProcAttr{
+			Credential: &syscall.Credential{
+				Uid: uid,
+			},
+		}
+	}
+	if err := cmd.Start(); err == nil {
+		writePidFile(pid, os.Getpid())
 		cmd.Wait()
 		ch <- struct{}{}
 	} else {
+		fmt.Println("create child process failed")
 		os.Exit(1)
 	}
 }
