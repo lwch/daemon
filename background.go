dc2daab3e40816aff99c54d25c0ce019b2ee999a
lwch <liwenchao@jkstack.com>
2020-12-30 14:17:26 +0800

    支持删除pid文件

--- background.go
+++ background.go
@@ -5,10 +5,28 @@ import (
 	"io/ioutil"
 	"os"
 	"os/exec"
+	"os/signal"
+	"syscall"
 )
+
+var cmd *exec.Cmd
 
 // Start start daemon
 func Start(pid string, arg ...string) {
+	if len(pid) > 0 {
+		c := make(chan os.Signal)
+		signal.Notify(c, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+		go func() {
+			<-c
+			os.Remove(pid)
+		}()
+	}
+	for {
+		run(pid, arg...)
+	}
+}
+
+func run(pid string, arg ...string) {
 	cmd := exec.Command(os.Args[0], arg...)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
@@ -18,7 +36,7 @@ func Start(pid string, arg ...string) {
 			writePidFile(pid, cmd.Process.Pid)
 		}
 		cmd.Process.Release()
-		os.Exit(0)
+		cmd.Wait()
 	}
 }
 
